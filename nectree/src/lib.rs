9b85c5bb450ffa28876fedf95d3ed91059da944e
bitful-pannul <bitful-pancreas@protonmail.com>
2024-01-02 15:59:37 -0300

    skeleton working

--- nectree/src/lib.rs
+++ nectree/src/lib.rs
@@ -1,9 +1,14 @@
-use serde::{Serialize, Deserialize};
-use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
+use std::collections::{BTreeMap, HashMap};
 use uqbar_process_lib::{
-    await_message, println, Address, Message, ProcessId, Request, Response, get_payload,
-    http::{IncomingHttpRequest, HttpServerRequest, StatusCode, send_response, serve_index_html, bind_http_static_path, bind_http_path},
-    vfs::open_file,
+    await_message, get_payload, get_state,
+    http::{
+        bind_http_path, bind_http_static_path, send_response, serve_index_html, HttpServerRequest,
+        IncomingHttpRequest, StatusCode,
+    },
+    println, set_state,
+    vfs::{open_file, File},
+    Address, Message, ProcessId, Request, Response,
 };
 
 wit_bindgen::generate!({
@@ -29,17 +34,13 @@ enum LinkRequest {
     Delete { name: String },
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-enum LinkResponse {
-    Get { link_tree: LinkTree },
-}
-
 type LinkTree = HashMap<String, Link>;
 
-fn handle_http_server_request (
+fn handle_http_server_request(
     our: &Address,
     ipc: &[u8],
-    mut link_tree: &mut LinkTree,
+    link_tree: &mut LinkTree,
+    html_file: &mut File,
 ) -> anyhow::Result<()> {
     let Ok(server_request) = serde_json::from_slice::<HttpServerRequest>(ipc) else {
         println!("nectree: couldn't parse request!");
@@ -49,21 +50,29 @@ fn handle_http_server_request (
     match server_request {
         HttpServerRequest::Http(IncomingHttpRequest { method, .. }) => {
             match method.as_str() {
-                // Get a path
-                "GET" => {
-                    let mut headers = HashMap::new();
-                    headers.insert("Content-Type".to_string(), "application/json".to_string());
-                    
-                    // Send an http response via the http server
-                }
-                // Send a message
                 "POST" => {
                     let Some(payload) = get_payload() else {
                         println!("no payload in nectree POST request");
+                        // TODO, send http_responses too?
                         return Ok(());
                     };
 
-                    // Send an http response via the http server
+                    let Ok(link_request) = serde_json::from_slice::<LinkRequest>(&payload.bytes) else {
+                        println!("nectree: couldn't parse link request!");
+                        return Ok(());
+                    };
+
+                    match link_request {
+                        LinkRequest::Save(link) => {
+                            link_tree.insert(link.name.clone(), link);
+
+                            save_and_render_html(our, link_tree, html_file)?;
+                        }
+                        LinkRequest::Delete { name } => {
+                            link_tree.remove(&name);
+                            save_and_render_html(our, link_tree, html_file)?;
+                        }
+                    }
                     send_response(StatusCode::CREATED, None, vec![])?;
                 }
                 _ => {
@@ -78,13 +87,70 @@ fn handle_http_server_request (
         }
     };
 
-Ok(())
-    
+    Ok(())
+}
+
+fn save_and_render_html(
+    our: &Address,
+    link_tree: &LinkTree,
+    html_file: &mut File,
+) -> anyhow::Result<()> {
+    // Save the LinkTree to state
+    let state = serde_json::to_vec(link_tree)?;
+    set_state(&state);
+
+    let mut links: Vec<&Link> = link_tree.values().collect();
+    links.sort_by_key(|link| link.order);
+
+    let html_links: String = links
+        .iter()
+        .map(|link| {
+            format!(
+                "<li><a href=\"{}\"><img src=\"{}\" alt=\"{}\">{}</a></li>",
+                link.url, link.image, link.description, link.name
+            )
+        })
+        .collect();
+
+    let html_body = format!("<ul>\n{}\n</ul>", html_links);
+    let html_header = generate_html_header();
+    let html = format!("{}{}\n</body>\n</html>", html_header, html_body);
+
+    // Write the HTML string to the file
+    html_file.write(html.as_bytes())?;
+    // might not be necessary
+    html_file.sync_all()?;
+
+    serve_index_html(our, "ui")?;
+
+    Ok(())
+}
+
+fn generate_html_header() -> String {
+    let html_header = r#"
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>NecTree</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+            }
+            h2 {
+                color: #333;
+            }
+        </style>
+    </head>
+    <body>
+        <h2>NecTree</h2>
+    "#;
+    html_header.to_string()
 }
 
 fn handle_message(
     our: &Address,
     mut link_tree: &mut LinkTree,
+    mut html_file: &mut File,
 ) -> anyhow::Result<()> {
     let message = await_message()?;
 
@@ -93,20 +159,17 @@ fn handle_message(
             println!("nnotes: got response - {:?}", message);
             return Ok(());
         }
-        Message::Request {
-            ref ipc,
-            ..
-        } => {
-            // Requests that come from our http server, handle intranode later too. 
-            handle_http_server_request(our, ipc, &mut link_tree)?;
-
+        Message::Request { ref ipc, .. } => {
+            // Requests that come from our http server, handle intranode later too.
+            handle_http_server_request(our, ipc, &mut link_tree, &mut html_file)?;
         }
     }
 
     Ok(())
 }
 
-/// 1. read in html/markdown file 
+// can make stateless, you'll deal with the html parsing sir.
+/// 1. read in html/markdown file
 /// 2. parse it into a tree
 
 /// 1. make linktree into an html/markdown file
@@ -119,18 +182,32 @@ impl Guest for Component {
         println!("nectree: begin");
 
         let our = Address::from_str(&our).unwrap();
-        let mut link_tree: LinkTree = HashMap::new();
 
-        // Bind the path, and serve the index.html file
+        // Get previous LinkTree or create a new one
+        let mut link_tree: LinkTree = match get_state() {
+            Some(state) => serde_json::from_slice::<LinkTree>(&state).unwrap(),
+            None => HashMap::new(),
+        };
+
+        // open existing html file, read it's bytes.
+        let mut html_file =
+            open_file(&format!("{}/pkg/ui/index.html", our.package_id()), false).unwrap();
+
+        // let html = html_file.read().unwrap();
+
+        // serve the "static" index.html file
+        // quick note, we can't use bind_path and bind static path together.
+
         serve_index_html(&our, "ui").unwrap();
-        bind_http_path("/", false, false).unwrap();
+        // bind the "/" path for post requests. 
+        //bind_http_path("/", false, false).unwrap();
 
         loop {
-            match handle_message(&our, &mut link_tree) {
-                Ok(()) => {},
+            match handle_message(&our, &mut link_tree, &mut html_file) {
+                Ok(()) => {}
                 Err(e) => {
                     println!("nectree: error: {:?}", e);
-                },
+                }
             };
         }
     }
